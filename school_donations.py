bf12517f9f3148544935962da472f281ba1943bd
anarchyroot <griewing15>
2017-07-11 12:49:26 +0100

    updated caption and link titles

--- school_donations.py
+++ school_donations.py
@@ -15,7 +15,7 @@ def index():
     """
     A Flask view to serve the main dashboard page.
     """
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/<collection>")
@@ -45,4 +45,4 @@ def get_valuesCollection(collection):
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='51.255.83.181', port=5000, debug=True)
